eebe03a040cc48e68050dc44fdac10f202cc34ec
Frederico Miranda Brandão Alves <frederico.miranda.brandao@gmail.com>
2020-01-16 19:47:02 -0300

    Add column to events model and migrate database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200116181511) do
+ActiveRecord::Schema.define(version: 20200116224130) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20200116181511) do
     t.integer "host_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date"
   end
 
   create_table "users", force: :cascade do |t|
