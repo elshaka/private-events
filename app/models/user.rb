52d1b1896227734fea4f9aabbd54e68b5396d93f
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-01-16 16:42:53 -0400

    Added user create (signup) and show functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :hosted_events, class_name: 'Event', foreign_key: :host_id
   has_many :attendances
   has_many :attending_events, through: :attendances, source: :event
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 16 }
 end
