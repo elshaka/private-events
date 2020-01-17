936719510c5f7a49dd8924d8739096dee3cf880c
Frederico Miranda Brandão Alves <frederico.miranda.brandao@gmail.com>
2020-01-17 12:18:32 -0300

    Add scopes to Event model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: 'User'
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
+  
+  scope :upcoming, -> { where('date >= ?', Time.now) }
+  scope :past, -> { where('date < ?', Time.now) }
 end
