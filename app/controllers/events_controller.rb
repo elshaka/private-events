206eeac746c2137b1d804a6f3f32abb52362c2ad
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-01-17 13:03:22 -0400

    Minor refactor in events controller. Added styling to user profile and new events view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
-class EventsController < ApplicationController    
-  before_action :user_logged_in? , only: [:create]
+class EventsController < ApplicationController
+  before_action :user_logged_in? , only: [:new, :create]
 
   def index
     @prev_events = Event.past
@@ -10,14 +10,22 @@ class EventsController < ApplicationController
     @event = Event.find_by(id: params[:id])
   end
 
-  def create
-    @event = current_user.hosted_events.build event_params 
-    @event.save
-    redirect_to @event
+  def new
+    @event = Event.new
   end
-  
+
+  def create
+    @event = current_user.hosted_events.build event_params
+    if @event.save
+      flash[:success] = 'Event created successfully'
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
   private
-  
+
   def event_params
     params.require(:event).permit(:description, :date)
   end
